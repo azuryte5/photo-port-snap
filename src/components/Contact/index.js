e41affe35f70d87a21ff3f2ca507abc59c135398
azuryte5 <85147307+azuryte5@users.noreply.github.com>
2022-01-05 16:26:37 -0500

    state changes with forms and submit

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,23 +1,34 @@
-import React from "react";
+import React, {useState} from "react";
 
 function ContactForm() {
+    //is the ability to initialize the values of the state. In this case, we want to clear the input fields on the component loading.
+    const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+    const { name, email, message } = formState;
 
-    // JSX
+    function handleChange(e) {
+        setFormState({...formState, [e.target.name]: e.target.value})
+        // console.log(formState)} <- How to see what is happening every change of state.
+    }
+    function handleSubmit(e) {
+    e.preventDefault();
+    console.log(formState);
+}
+        // JSX
     return (
         <section>
             <h1>Contact me</h1>
-            <form id="contact-form">
+            <form id="contact-form" onSubmit={handleSubmit}>
                 <div>
                     <label htmlFor="name">Name:</label>
-                    <input type='text' name="name" />
+                    <input type='text' name="name" defaultValue={name} onChange={handleChange}/>
                 </div>
                 <div>
                     <label htmlFor="email">Email Address:</label>
-                    <input type='email' name="email"/> 
+                    <input type='email' name="email"defaultValue={email} onChange={handleChange}/> 
                 </div>
                 <div>
                     <label htmlFor="message">Message:</label>
-                    <textarea type='message' row="5"/> 
+                    <textarea name="message" defaultValue={message} onChange={handleChange} rows="5" />
                 </div>
                 <button type="submit">Submit</button>
             </form>
